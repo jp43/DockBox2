6d03545c83fada8a7e353525acfa64fbbbeac9a7
jordane.preto <jordane.preto@gpu-003.dc.p2chpd.univ-lyon1.fr>
2022-10-20 23:42:56 +0200

    build dataset classe and started model class

--- main.py
+++ main.py
@@ -10,42 +10,100 @@ import tensorflow as tf
 import loss
 import utils
 
-nrof_neigh_per_batch = 25
 depth = 2
+nrof_neigh_per_batch = 25
+activiation = 'sigmoid'
 
-def __sample(graph_id):
-    graph_size = train['Graph_size'][graph_id]
-    adj = train['Graph_adj_list'][graph_id]
-    diff = train['max_nrof_nodes'] - graph_size
+class GraphDataset(object):
 
-    if diff > 0:
-        vertices = np.concatenate([train['Graph_feats'][graph_id], np.zeros((diff, nfeats), dtype=np.float32)], axis=0)
-        labels = np.concatenate([train['Graph_labels'][graph_id], np.zeros((diff, nlabels), dtype=np.int32)], axis=0)
-    else:
-        vertices = train['Graph_feats'][graph_id]
-        labels = train['Graph_labels'][graph_id]
+    def __init__(self, filename):
 
-    indices_all, adj_mask_all, nneigh_all = utils.generate_neighbours(adj, graph_size, depth, nrof_neigh_per_batch)
-    sys.exit()
+        with open(filename, "rb") as ff:
+           data = pickle.load(ff)
+ 
+        self.graph_feats = data['Graph_feats']
+        self.graph_adj_list = data['Graph_adj_list']
+        self.graph_labels = data['Graph_labels']
+        self.max_nrof_nodes = data['max_nrof_nodes']
 
-def sample(graph_id):
-    return tf.py_function(__sample, [graph_id], tf.int64)
+        self.nfeats = data['Graph_feats'][0].shape[1]
 
-with open('datasets/data_ppi/train_ppi.pickle', "rb") as ff:
-   train = pickle.load(ff)
+        self.nlabels = data['Graph_labels'][0].shape[1]
+        self.ngraphs = len(data['Graph_nodes'])
 
-train['Graph_size'] = []
-for i_g, graph in enumerate(train['Graph_nodes']):
-    train['Graph_size'].append(len(graph))
+        self.graph_size = []
+        for i_g, graph in enumerate(data['Graph_nodes']):
+            self.graph_size.append(len(graph))
 
-nfeats = train['Graph_feats'][0].shape[1]
-nlabels = train['Graph_labels'][0].shape[1]
+    def __sample(self, graph_id, depth, nrof_neigh_per_batch):
 
-data_slices = np.random.permutation(len(train['Graph_nodes']))
+        graph_size = self.graph_size[graph_id]
+        adj = self.graph_adj_list[graph_id]
+        diff = self.max_nrof_nodes - graph_size
+
+        depth = tf.get_static_value(depth)
+        nrof_neigh_per_batch = tf.get_static_value(nrof_neigh_per_batch)  
+
+        if diff > 0:
+            feats = np.concatenate([self.graph_feats[graph_id], np.zeros((diff, self.nfeats), dtype=np.float32)], axis=0)
+            labels = np.concatenate([self.graph_labels[graph_id], np.zeros((diff, self.nlabels), dtype=np.int32)], axis=0)
+        else:
+            feats = self.graph_feats[graph_id]
+            labels = self.graph_labels[graph_id]
+    
+        neigh_indices, adj_mask, nneigh = utils.sample_neighbors(adj, graph_size, depth, nrof_neigh_per_batch)
+    
+        for idx in range(depth):
+            if diff > 0:
+                # zero padding for each layer until getting max_nrof_nodes nodes per graph
+                neigh_indices[idx] = np.concatenate([neigh_indices[idx], np.zeros((diff, nrof_neigh_per_batch), dtype=np.int32)], axis=0)
+                adj_mask[idx] = np.concatenate([adj_mask[idx], np.zeros((diff, nrof_neigh_per_batch), dtype=np.int32)], axis=0)
+                nneigh[idx] = np.concatenate([nneigh[idx], np.zeros((diff), dtype=np.int32)], axis=0)
+    
+        return np.asarray(feats, np.float32), graph_size, \
+               np.asarray(neigh_indices, dtype=np.int32), np.asarray(adj_mask, dtype=np.float32), \
+               np.asarray(nneigh, dtype=np.int32), labels
+    
+    def sample(self, graph_id, depth, nrof_neigh_per_batch):
+
+        return tf.py_function(self.__sample, [graph_id, depth, nrof_neigh_per_batch], [tf.float32, tf.int32,
+                                   tf.int32, tf.float32, tf.int32, tf.int32])
+
+class GraphSage(tf.keras.models.Model):
+
+    def __init__(self, in_shape, out_shape, activation,
+                 aggregator_layers,
+                 loss_cls, accuracy_cls,
+                 train_cfg, test_cfg):
+        super(GraphSAGE, self).__init__()
+
+        self.in_shape = in_shape
+        self.out_shape = out_shape
+
+        self.activation = activation
+        if self.activation is not None:
+            self.activation = getattr(tf.nn, activation)
+
+        self.aggregator_layers = build_aggregator_layers(aggregator_layers)
+
+        self.loss_graph = build_loss(loss_cls)
+
+        self.accuracy_cls = accuracy_cls
+        self.accuracy = build_accuracy(accuracy_cls)
+
+        self.train_cfg = train_cfg
+        self.test_cfg = test_cfg
+
+
+    def build(self, *args):
+        super(GraphSage, self).build()
+ 
+train = GraphDataset('datasets/data_ppi/train_ppi.pickle')
+
+data_slices = np.random.permutation(train.ngraphs)
 data_loader = tf.data.Dataset.from_tensor_slices((data_slices))
 
-data_loader = data_loader.map(**{'num_parallel_calls': 4, 'map_func': sample})
+data_loader = data_loader.map(**{'num_parallel_calls': 4, 'map_func': lambda x: train.sample(x, depth, nrof_neigh_per_batch)})
 
-for tensor in data_loader:
-    pass
+#model = GraphSage(2*train.nfeats, train.nlabels, activation)
 
--- utils.py
+++ utils.py
@@ -1,39 +1,37 @@
 import numpy as np
 
-def generate_neighbours(adj, graph_size, depth, nrof_neigh_per_batch):
-    indices_all = []
-    adj_mask_all = []
-
-    nneigh_all = []
+def sample_neighbors(adj, graph_size, depth, nrof_neigh_per_batch):
+    neigh_indices = []
+    adj_mask = []
+    nneigh = []
 
     for idx in range(depth):
-        indices = []
-        nneigh = []
-        adj_mask = []
+        graph_neigh_indices = []
+        graph_adj_mask = []
+        graph_nneigh = []
 
         for jdx in range(graph_size):
             # get indices of neighbors of node jdx 
-            neigh_indices = np.where(adj[jdx] != 0)[0]
+            node_neigh_indices = np.where(adj[jdx] != 0)[0]
 
             # get indices of non neighbors
-            non_neigh_indices = np.setdiff1d(np.arange(graph_size), neigh_indices)
+            node_non_neigh_indices = np.setdiff1d(np.arange(graph_size), node_neigh_indices)
 
-            if len(neigh_indices) > nrof_neigh_per_batch:
-                neigh_indices = np.random.choice(neigh_indices, nrof_neigh_per_batch, replace=False)
+            # if number of neighbors is too high, select randomly until having "nrof_neigh_per_batch" neighbors
+            if len(node_neigh_indices) > nrof_neigh_per_batch:
+                node_neigh_indices = np.random.choice(node_neigh_indices, nrof_neigh_per_batch, replace=False)
 
-            _nneigh = len(neigh_indices)
-            if _nneigh < nrof_neigh_per_batch:
-                neigh_indices = np.concatenate([neigh_indices, [-1]*(nrof_neigh_per_batch - _nneigh)])
+            node_nneigh = len(node_neigh_indices)
+            if node_nneigh < nrof_neigh_per_batch: # zero padding until getting "nrof_neigh_per_batch" neighbors
+                node_neigh_indices = np.concatenate([node_neigh_indices, [node_non_neigh_indices[0]]*(nrof_neigh_per_batch - node_nneigh)])
 
-            neigh_adj_nodes = adj[jdx][neigh_indices]
-            indices.append(neigh_indices)
+            graph_neigh_indices.append(node_neigh_indices)
+            graph_adj_mask.append(adj[jdx][node_neigh_indices])
+            graph_nneigh.append(node_nneigh)
 
-            nneigh.append(_nneigh)
-            adj_mask.append(neigh_adj_nodes)
+        neigh_indices.append(graph_neigh_indices)
+        adj_mask.append(graph_adj_mask)
+        nneigh.append(graph_nneigh)
 
-        indices_all.append(indices)
-        adj_mask_all.append(adj_mask)
-        nneigh_all.append(nneigh)
-
-    return indices_all, adj_mask_all, nneigh_all
+    return neigh_indices, adj_mask, nneigh
 
